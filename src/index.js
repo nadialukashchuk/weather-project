ffb91010a264caaf65a08bf688edf82ce6744498
Nadia <127159888+nadialukashchuk@users.noreply.github.com>
2023-12-08 22:53:52 +0100

    Added the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -54,3 +54,36 @@ let currentDate = new Date();
 
 
 currentDateElement.innerHTML = formatDate(currentDate);
+
+
+
+function displayForecast (){
+    let forecastElement = document.querySelector("#forecast");
+
+
+    let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    let forecastHtml = "";
+    days.forEach(function (day) {
+    
+    forecastHtml = forecastHtml + 
+     `
+
+    <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">:D</div>
+        <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>12°</strong>
+         </div>
+         <div class="weather-forecast-temperature">23°</div>
+         </div>
+        </div>
+`;
+});
+forecastElement.innerHTML = forecastHtml
+
+}
+
+    
+
+displayForecast()
